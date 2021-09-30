a33b550e3f350f21368ca6ac11651c0bbb977e8a
toyo <yuma@happy-quality.jp>
2021-09-30 17:17:13 +0900

    [fix] sshpass option order

--- sshpass.sh
+++ sshpass.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-ssh-keyscan -H ${1:-192.168.11.150} -p ${2:-22} >> ~/.ssh/known_hosts
+ssh-keyscan -H -p ${2:-22} ${1:-192.168.11.150} >> ~/.ssh/known_hosts
 sshpass -p password ssh -l pi ${1:-192.168.11.150}
 
