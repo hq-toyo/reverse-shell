dcae9e6d18d9d17cd8ec6cd05b695cf886f3b929
toyo <yuma@happy-quality.jp>
2021-09-30 16:51:55 +0900

    [update] executable ./sshpass.sh ip port

--- sshpass.sh
+++ sshpass.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-ssh-keyscan 192.168.11.150 >> ~/.ssh/known_hosts
-sshpass -p password ssh -l pi 192.168.11.150
+ssh-keyscan -H ${1:-192.168.11.150} -p ${2:-22} >> ~/.ssh/known_hosts
+sshpass -p password ssh -l pi ${1:-192.168.11.150}
 
